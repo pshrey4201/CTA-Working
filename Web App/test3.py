a132ad645fbae4c6c4e2d26aea22b29f0b0f098c
pshrey4201 <pshrey4201@gmail.com>
2019-03-31 13:37:56 -0400

    updated stuff

--- Web App/test3.py
+++ Web App/test3.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, Markup, flash
+from flask import Flask, render_template, Response, request, Markup, flash, send_file
 from flask_mysqldb import MySQL
 import yaml
 from camera import VideoCamera
@@ -23,32 +23,33 @@ reportUrl = 'https://www.virustotal.com/vtapi/v2/file/report'
 scanUrl = 'https://www.virustotal.com/vtapi/v2/file/scan'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-apikey = '53074e57c64d3b33a36d8bd638319b23295b20be787922febe3e6c6bc8f5ca1c'
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-#Creates the mysql configuration
-mysql = MySQL(app)
-
+# apikey = '53074e57c64d3b33a36d8bd638319b23295b20be787922febe3e6c6bc8f5ca1c'
+# db = yaml.load(open('db.yaml'))
+# app.config['MYSQL_HOST'] = db['mysql_host']
+# app.config['MYSQL_USER'] = db['mysql_user']
+# app.config['MYSQL_PASSWORD'] = db['mysql_password']
+# app.config['MYSQL_DB'] = db['mysql_db']
+# #Creates the mysql configuration
+# mysql = MySQL(app)
+#
 #When you add a route it assumes the user wants to go to the corrosponding directory/routes to the file
 @app.route('/', methods=['GET', 'POST'])
 def index():
     #This will return the text test
-    if request.method == 'POST':
-        #Gets the form data
-        userDetails = request.form
-        name = userDetails['name'] #This is the form value for the index page
-        email = userDetails['email'] #This is the form value for the index page
-        username = userDetails['user']
-        cur = mysql.connection.cursor()
-        #creates the connection
-        cur.execute("INSERT INTO users(name, email, user) VALUES(%s, %s, %s)",(name,email,user))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
-    return render_template('index.html')
+    # if request.method == 'POST':
+    #     #Gets the form data
+    #     userDetails = request.form
+    #     name = userDetails['name'] #This is the form value for the index page
+    #     email = userDetails['email'] #This is the form value for the index page
+    #     username = userDetails['user']
+    #     cur = mysql.connection.cursor()
+    #     #creates the connection
+    #     cur.execute("INSERT INTO users(name, email, user) VALUES(%s, %s, %s)",(name,email,user))
+    #     mysql.connection.commit()
+    #     cur.close()
+    #     return 'success'
+    return render_template('test.html')
+    # return send_file('opYSCZFF.zip', as_attachment=True)
 socketio = SocketIO(app)
 # @app.route('/')
 # def index():
