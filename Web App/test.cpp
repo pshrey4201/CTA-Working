b1771a6c2be5f9108cbc7dbef6ccc915d70e9bc8
Shrey Patel <patelshrey4201@gmail.com>
2019-04-23 14:48:17 -0400

    Create test.cpp

---
+++ Web App/test.cpp
@@ -0,0 +1,55 @@
+#include <C:/VulkanSDK/1.1.106.0/Include/vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class HelloTriangleApplication {
+  const int WIDTH = 800;
+  const int HEIGHT = 600;
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initWindow() {
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+      window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+      while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+      }
+    }
+
+    void cleanup() {
+      glfwDestroyWindow(window);
+      glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
